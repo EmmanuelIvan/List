68a37dcc2d740db4486b5f58763240d1e50fa1c5
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-05-01 03:44:49 +0000

    Create List_delete.py

---
+++ List_delete.py
@@ -0,0 +1,7 @@
+#Veremos como eliminar datos de una lista
+
+listas = ["Emmanuel", "Ivan", "Arrieta", "Rios"]
+print(listas)
+nombre = raw_input("Que nombre deseas eliminar: ")
+listas.remove(nombre)
+print(listas)
