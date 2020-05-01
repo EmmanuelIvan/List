075f104153074fc7ffcc64f3a65fee392d87983d
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-05-01 03:43:42 +0000

    Create List_update.py

---
+++ List_update.py
@@ -0,0 +1,8 @@
+#Veremos como modificar datos de una lista 
+
+lista = [1, 2, 3, 4, 5]
+print(lista)
+numero = int(input("QUe numero deseas modificar: "))
+numero2 = int(input("Que numero deseas ingresar: "))
+lista[numero] = numero2
+print(lista)
